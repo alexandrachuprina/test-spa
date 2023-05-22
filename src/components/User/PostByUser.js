4a59963122ef7cbbb9f40b47f7a311e34a8e3a87
shplvk <114808997+shplvk@users.noreply.github.com>
2023-05-22 13:28:53 +0300

    feat: delay & loading animation in PostByUser component

--- src/components/User/PostByUser.js
+++ src/components/User/PostByUser.js
@@ -5,16 +5,24 @@ import { getPostId, getComments } from "../../features/commentsSlice";
 import { setUserData } from "../../features/userSlice";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Spinner } from "react-bootstrap";
 import Comment from "../Comment/Comment";
 
 function PostByUser({ title, text, postId }) {
   const allComments = useSelector((state) => state.comments.comments);
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setComments(allComments);
   }, [showComments]);
+
+  const setDelay = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+  };
 
   return (
     <Card style={{ width: "400px", margin: "2vh" }}>
@@ -25,21 +33,35 @@ function PostByUser({ title, text, postId }) {
           variant="primary"
           onClick={() => {
             setShowComments(!showComments);
+            setDelay();
           }}
         >
           Комментарии
         </Button>
-        {showComments ? (
-          <>
-            {comments.length === 0 ? <p>no comments bro</p> : null}
-
-            {comments
-              .filter((elem) => elem.postId === postId)
-              .map((elem) => (
-                <Comment title={elem.email} text={elem.body} key={elem.id} />
-              ))}
-          </>
-        ) : null}
+        <Card.Body>
+          {showComments ? (
+            <>
+              {isLoading ? (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              ) : (
+                <>
+                  {comments.length === 0 ? <p>no comments bro</p> : null}
+                  {comments
+                    .filter((elem) => elem.postId === postId)
+                    .map((elem) => (
+                      <Comment
+                        title={elem.email}
+                        text={elem.body}
+                        key={elem.id}
+                      />
+                    ))}
+                </>
+              )}
+            </>
+          ) : null}
+        </Card.Body>
       </Card.Body>
     </Card>
   );
